a7311fe7a52a4b629d03a84ce0aaa8c32beb1351
Paul Martin <martip8@rpi.edu>
2020-08-19 13:17:48 -0400

    Wrote a snippet to output my \Delta H for a cold start lattice and a hot start lattice.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
+#include<fstream>
 #include<complex>
+#include<cstdlib>
 #include "lattice.h"
 #include "rand.h"
 #include "sim.h"
@@ -7,35 +9,58 @@
 
 int main()
 {
-
-//set higgs to zero, link fields to diag{e^{i\phi},e^{-i\phi}}
-// matrix_complex A,B;
-// double phi= 3.141592/4.0;
-// complex<double> temp;
-// temp = std::exp( complex<double>(0.0d,phi)  );
-// A(0,0) = temp;
-// A(1,1) = std::conj(temp);
-// B.setZero();
-//
-// lattice_site my_site(A,B);
-// lattice my_L(my_site);
-// simulation my_sim(my_L);
-//
-// std::cout << "Higgs force: " << my_sim.georgiGlashowActionPhiDerivative(1,my_L);
-// std::cout << "Link force: " << my_sim.georgiGlashowActionLinkDerivative(1,1,my_L);
+  matrix_complex higgsZero, linkIdentity;
+  higgsZero.setZero(); linkIdentity.setIdentity();
+  lattice_site cold_site(linkIdentity, higgsZero);
+  lattice cold_lattice(cold_site);
 
   // //Initialize Simulation -> lattice
+
   simulation sim1;
-  sim1.initializeHMC();
-  //
+  simulation sim2(cold_lattice);
+  int stepsArray[3];
+  double HdiffArray1[3] = {0.0d}, HdiffArray2[3] = {0.0d};
+  for(int i = 0; i < 5;i++)
+  {
+    sim1.initializeHMC();
+    sim2.initializeHMC();
+  }
+
+
+  int current_steps = 10;
   // //Run HMC
-  for(int i=0; i < 10;i++)
-    sim1.runLeapfrogSimulation();
+  for(int j=0; j<3;j++)
+  {
+    stepsArray[j] = current_steps;
+    sim1.setupSteps(current_steps);
+    sim2.setupSteps(current_steps);
+    for(int i=0; i < 1;i++)
+    {
+      HdiffArray1[j] += std::abs(sim1.runLeapfrogSimulation());
+      HdiffArray2[j] += std::abs(sim2.runLeapfrogSimulation());
+    }
+    HdiffArray1[j] /= 1.0;
+    HdiffArray2[j] /= 1.0;
+    current_steps *= 10;
+  }
+
+ofstream hot_data, cold_data;
+hot_data.open("hot_data1.txt",std::ios::out);
+cold_data.open("cold_data1.txt",std::ios::out);
+for(int i = 0;i <4; i++)
+{
+  hot_data << stepsArray[i] << " " << HdiffArray1[i] << std::endl;
+  cold_data << stepsArray[i] << " " << HdiffArray2[i] << std::endl;
+}
+
+hot_data.close();
+cold_data.close();
     //std::cout << "Hamiltonian: " << sim1.Hamiltonian() << std::endl;
-  sim1.printAcceptance();
+
+  //sim1.printAcceptance();
 
   //Measure observables (average plaquette)
-  std::cout << "<Phi> = " << sim1.averagePhi() << std::endl;
-  std::cout << "<Phi^2> = " << sim1.averagePhi2() << std::endl;
-  std::cout << "Average plaquette = " << sim1.averagePlaquettes() << std::endl;
+  // std::cout << "<Phi> = " << sim1.averagePhi() << std::endl;
+  // std::cout << "<Phi^2> = " << sim1.averagePhi2() << std::endl;
+  // std::cout << "Average plaquette = " << sim1.averagePlaquettes() << std::endl;
 }
