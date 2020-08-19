89333b05612bb2c4497cff2f77553a2d70b3c4c7
Paul <martip8@rpi.edu>
2020-08-19 18:04:54 -0400

    Wrapped the stop watch in a class.

--- src/stopwatch.cpp
+++ src/stopwatch.cpp
@@ -2,21 +2,19 @@
 #include <sys/time.h>
 #include "stopwatch.h"
 
-struct timeval myStartTime;
 
-void stopwatchStart()
+
+void mytime::stopwatchStart()
 {
 	gettimeofday(&myStartTime, NULL);
 }
 
-double stopwatchReadSeconds()
+double mytime::stopwatchReadSeconds()
 {
 	struct timeval endTime;
 	gettimeofday(&endTime, 0);
-    
+
 	long ds = endTime.tv_sec - myStartTime.tv_sec;
 	long dus = endTime.tv_usec - myStartTime.tv_usec;
 	return ds + 0.000001*dus;
 }
-
-
