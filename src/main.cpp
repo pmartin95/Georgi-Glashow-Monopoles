f138c56ed3318157883163db8297fb52e25ec677
Paul Martin <pmartin5.phy@gmail.com>
2021-02-13 10:12:22 -0500

    Fixed the energy counter. It wasn't being set to 0 between iterations.

--- src/main.cpp
+++ src/main.cpp
@@ -23,21 +23,32 @@ int main()
 
 
         std::cout << "Initialization complete.\n";
-        int temp_step = 10;
-        double temp_hdiff = 0.0d;
+        int temp_step = 2;
+        double temp_hdiff;
         int i,j;
 
-        for(i=0; i<3; i++)
+        for(i=0; i<10; i++)
         {
+                temp_hdiff = 0.0d;
                 N_Steps.push_back(temp_step);
                 sim1.setupSteps(temp_step);
                 std::cout << "Performing " << j << " runs with " << temp_step << " intervals\n";
                 for(j=0; j<30; j++)
+                {
+                        if(j%5==0)
+                                std::cout << temp_step << " steps per sweep. " << j << "/29 runs.\n";
                         temp_hdiff += abs( sim1.runLeapfrogSimulation() );
+                }
+                std::cout << "Average Phi: " << sim1.averagePhi()<<  '\n';
+                std::cout << "Average Phi Squared: " << sim1.averagePhi2()<<  '\n';
+                std::cout << "Average plaquette: " << sim1.averagePlaquettes()  <<  '\n';
+                sim1.printAcceptance();
+                sim1.resetAcceptanceCounter();
+
                 std::cout << "step size: " << sim1.stepSize << std::endl;
                 std::cout << "On to the next one!" << std::endl;
-                H_diff_average.push_back(temp_hdiff/static_cast<double>(j));
-                temp_step *= 10;
+                H_diff_average.push_back(temp_hdiff/30.0);
+                temp_step *= 2;
 
         }
 
