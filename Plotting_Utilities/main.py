2b034e05cc77ffb13a5586fcf0113ffde2ee541a
Paul Martin <pmartin5.phy@gmail.com>
2021-03-26 13:10:13 -0400

    Modified my printing utility to group by type

--- Plotting_Utilities/main.py
+++ Plotting_Utilities/main.py
@@ -13,17 +13,28 @@ import numpy as np
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    data = np.loadtxt('./../src/Hdiffdata.txt')
-    x = data[:,0]
-    y = data[:,1]
+
+    data_dict = {}
+
+    with open("HMCsim1.txt", 'r') as datafile:
+        lines = datafile.readlines()
+        for line in lines:
+            l = line.split()
+            if l[0] in data_dict.keys():
+                data_dict[l[0]].append(l[3])
+            else:
+                data_dict[l[0]] = [l[3]]
+
+    del data_dict["#g"]
+    print(data_dict)
     #x = 1.0 / x
-    plt.plot(x,y)
-    plt.yscale("log")
-    plt.xscale("log")
-    plt.xlabel("Number of Steps")
-    plt.ylabel(r'$\Delta H$')
-    plt.savefig("HdiffPlot.png")
-    plt.show()
+    # plt.plot(x,y)
+    # plt.yscale("log")
+    # plt.xscale("log")
+    # plt.xlabel("Number of Steps")
+    # plt.ylabel(r'$\Delta H$')
+    # plt.savefig("HdiffPlot.png")
+    # plt.show()
 
 
 
