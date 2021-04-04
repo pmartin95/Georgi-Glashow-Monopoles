2886ff5113dd930ae4275f322e96cece4923efef
Paul Martin <pmartin5.phy@gmail.com>
2021-04-04 17:46:23 -0400

    Added routines for testing jackknife

--- src/main.cpp
+++ src/main.cpp
@@ -12,22 +12,36 @@
 #include "lattice.h"
 #include "rand.h"
 #include "sim.h"
+#include "statistics.h"
 #include "stopwatch.h"
 #include <unsupported/Eigen/MatrixFunctions>
 
 int main()
 {
 
-        std::string filename = "annealingSchedule.txt";
-        simulation sim1;
-        sim1.switchDSV();
-        sim1.inputScheduleParameters(filename);
-        void runHMCSimulationSchedule(int init_iter,int iter, int iter_measure);
-        sim1.runHMCSimulationSchedule(30,600,20);
-        sim1.printDataFile("HMCsim1.txt");
-        // sim1.resetDataPoints();
-        // std::cout << "Finished running hybrid Monte Carlo.\n";
-        // sim1.runMHMCSimulationSchedule(2000,100);
-        // sim1.printDataFile("MHMCsim1.txt");
+        std::vector<double> testvector;
+        std::mt19937_64 g(seedGen());
+        std::normal_distribution<double> distribution(0.0,1.0);
+        double jack_error, jack_ave;
+        vectorFunc f = cosave;
+        for(int i =0; i < 1000; i++)
+        {
+                testvector.push_back( distribution(g) + M_PI /3.0  ); // +
+        }
+        std::cout << "Here.\n";
+        if(!computeJackknifeStatistics(testvector,f,10,jack_ave,jack_error))
+                std::cout << "Jackknife succeeded.\n"
+                          << "Average: " << jack_ave << std::endl
+                          <<"Error: " << jack_error << std::endl;
+
+
+        std::cout << '\a';
         return 0;
+}
+
+double cosave(const std::vector<double>& in)
+{
+        double temp;
+        temp = average(in);
+        return cos(temp);
 }
