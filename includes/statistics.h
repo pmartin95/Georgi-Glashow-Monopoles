ee90dab259230d4767e60e69b8abc9728229f0ef
Paul Martin <pmartin5.phy@gmail.com>
2021-04-04 17:44:05 -0400

    Added a file to handle my statistics

---
+++ includes/statistics.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#ifndef __STATISTICS__
+
+typedef double (*vectorFunc) (const std::vector<double>& );
+
+#include <cstdlib>
+#include <iomanip>
+#include <sys/time.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <complex>
+#include <cstdlib>
+#include <cmath>
+#include <vector>
+#include <omp.h>
+#include "lattice.h"
+#include "rand.h"
+#include "sim.h"
+#include "statistics.h"
+#include "stopwatch.h"
+#include <unsupported/Eigen/MatrixFunctions>
+
+#define __STATISTICS__
+
+
+enum jackknife{JACKKNIFE_SUCCESS, JACKKNIFE_NO_DIVIDE};
+double average(const std::vector<double>& input);
+double sumVec(const std::vector<double>& input);
+
+int computeJackknifeStatistics(const std::vector<double>& inputData, int setLength, double& Jackknife_ave, double& Jackknife_error );
+int computeJackknifeStatistics(const std::vector<double>& inputData, vectorFunc f,  int setLength, double& Jackknife_ave, double& Jackknife_error );
+double cosave(const std::vector<double>& in);
+#endif
