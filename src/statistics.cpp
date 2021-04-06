9a6083c4e7916f01cde586dd9b83db42422cf27f
Paul Martin <pmartin5.phy@gmail.com>
2021-04-05 23:00:00 -0400

    Wrote jackknife routines that accept functions

--- src/statistics.cpp
+++ src/statistics.cpp
@@ -34,7 +34,7 @@ double sumVec(const std::vector<double>& input)
 }
 int computeJackknifeStatistics(const std::vector<double>& inputData, int setLength, double& Jackknife_ave, double& Jackknife_error )
 {
-        return computeJackknifeStatistics(inputData,&average,setLength,Jackknife_ave,Jackknife_error);
+        return computeJackknifeStatistics(inputData,average,setLength,Jackknife_ave,Jackknife_error);
 }
 
 int computeJackknifeStatistics(const std::vector<double>& inputData, vectorFunc f,  int setLength, double& Jackknife_ave, double& Jackknife_error )
@@ -65,4 +65,55 @@ int computeJackknifeStatistics(const std::vector<double>& inputData, vectorFunc 
         }
         Jackknife_error = sqrt( static_cast<double>(numSets-1) * average(deviations)  );
         return JACKKNIFE_SUCCESS;
+}
+
+
+int computeJackknifeStatistics(const std::vector<std::vector<double> >& inputData, vectorVectorFunc f,  int setLength, double& Jackknife_ave, double& Jackknife_error )
+{
+        if(inputData[0].size()%setLength != 0)
+                return JACKKNIFE_NO_DIVIDE;
+        std::vector<std::vector<double> > holdingVector;
+
+        int numSets;
+        numSets = inputData[0].size()/setLength;
+        std::vector<double> setOfAverages(numSets,0.0);
+        for(int i = 0; i < numSets; i++ )
+        {
+                for(int j = 0; j< inputData.size(); j++)
+                {
+                        holdingVector[j].clear();
+                        holdingVector[j].reserve(inputData[j].size()-setLength);
+                        holdingVector[j].insert(holdingVector[j].end(),inputData[j].begin(),inputData[j].begin() +i*setLength  );
+                        holdingVector[j].insert(holdingVector[j].end(),inputData[j].begin() + (i+1)*setLength,inputData[j].end());
+                }
+
+                setOfAverages[i]= f(holdingVector);
+        }
+        Jackknife_ave = average(setOfAverages);
+
+        std::vector<double> deviations(numSets,0.0);
+        double temp;
+        for(int i = 0; i < numSets; i++)
+        {
+                temp = setOfAverages[i] - Jackknife_ave;
+                deviations[i] = temp * temp;
+        }
+        Jackknife_error = sqrt( static_cast<double>(numSets-1) * average(deviations)  );
+        return JACKKNIFE_SUCCESS;
+}
+
+
+
+//First vector is W(R,R), then W(R,R-1), then W(R-1,R-1)
+double CreutzRatio(std::vector<std::vector<double> >rectangleData)
+{
+        if( rectangleData.size() != 3)
+                std::cout << "Data error: improper vector size.\n";
+        double WRR, WRR1, WR1R1;
+        WRR = average(rectangleData[0]);
+        WRR1 = average(rectangleData[1]);
+        WR1R1 = average(rectangleData[2]);
+        WRR1 = WRR1 * WRR1;
+
+        return -log(WRR * WR1R1/WRR1);
 }
