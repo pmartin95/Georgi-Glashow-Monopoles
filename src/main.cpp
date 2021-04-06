900f7c9b9cda78025654efd418079c20eddb0c56
Paul Martin <pmartin5.phy@gmail.com>
2021-04-05 23:01:07 -0400

    Wrote some examples to test the creutz ratio

--- src/main.cpp
+++ src/main.cpp
@@ -19,20 +19,23 @@
 int main()
 {
 
-        std::vector<double> testvector;
-        std::mt19937_64 g(seedGen());
-        std::normal_distribution<double> distribution(0.0,1.0);
-        double jack_error, jack_ave;
-        vectorFunc f = cosave;
-        for(int i =0; i < 1000; i++)
-        {
-                testvector.push_back( distribution(g) + M_PI /3.0  ); // +
-        }
-        std::cout << "Here.\n";
-        if(!computeJackknifeStatistics(testvector,f,10,jack_ave,jack_error))
-                std::cout << "Jackknife succeeded.\n"
-                          << "Average: " << jack_ave << std::endl
-                          <<"Error: " << jack_error << std::endl;
+        std::vector<std::vector<double> > v;
+        std::vector<double> b(10,0.0d);
+        v.push_back(b);
+        v.push_back(b);
+        v[0].push_back(5.0 );
+        std::cout << v[0].size() << std::endl;
+        // matrix_complex iden,zero;
+        // iden.setIdentity();
+        // zero.setZero();
+        // lattice_site site1(iden,zero);
+        // lattice L_in(site1);
+        // simulation sim1(L_in);
+        // simulation sim1;
+        // for(int i = 0; i < 100; i++)
+        //         sim1.initializeHMC();
+        // std::cout << sim1.CreutzRatio(1,1) << std::endl;
+        // std::cout << sim1.CreutzRatio(2,2) << std::endl;
 
 
         std::cout << '\a';
