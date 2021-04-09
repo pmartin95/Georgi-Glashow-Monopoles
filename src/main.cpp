4b959e69be64ebb269a42f559be8af336afa7e6d
Paul Martin <pmartin5.phy@gmail.com>
2021-04-09 17:41:21 -0400

    Added some code for debugging the indexing problem I'm having

--- src/main.cpp
+++ src/main.cpp
@@ -16,26 +16,38 @@
 #include "stopwatch.h"
 #include <unsupported/Eigen/MatrixFunctions>
 
+
+
 int main()
 {
 
-        std::vector<std::vector<double> > v;
-        std::vector<double> b(10,0.0d);
-        v.push_back(b);
-        v.push_back(b);
-        v[0].push_back(5.0 );
-        std::cout << v[0].size() << std::endl;
-        // matrix_complex iden,zero;
-        // iden.setIdentity();
-        // zero.setZero();
-        // lattice_site site1(iden,zero);
-        // lattice L_in(site1);
-        // simulation sim1(L_in);
+
+
         // simulation sim1;
-        // for(int i = 0; i < 100; i++)
-        //         sim1.initializeHMC();
-        // std::cout << sim1.CreutzRatio(1,1) << std::endl;
-        // std::cout << sim1.CreutzRatio(2,2) << std::endl;
+        // std::vector<std::vector<double> > temp;
+        // double temp_ave, temp_error;
+        //
+        //
+        //
+        // sim1.inputScheduleParameters("annealingSchedule.txt");
+        //
+        // sim1.runMHMCSimulationSchedule(2000, 100);
+        // for(int i =0; i < sim1.schedule.size(); i++)
+        // {
+        //         sim1.convertDataForJackknifeCreutz(sim1.schedule[i].g_value,2, temp);
+        //         computeJackknifeStatistics(temp[2], average,  2, temp_ave, temp_error );
+        //         std::cout << 4.0/sim1.schedule[i].g_value*sim1.schedule[i].g_value << " " << temp_ave<< " " << temp_error << std::endl;
+        // }
+        // sim1.resetDataPoints();
+        // sim1.runHMCSimulationSchedule(30,200,10);
+        // for(int i =0; i < sim1.schedule.size(); i++)
+        // {
+        //         sim1.convertDataForJackknifeCreutz(sim1.schedule[i].g_value,2, temp);
+        //         computeJackknifeStatistics(temp[2], average,  2, temp_ave, temp_error );
+        //         std::cout << 4.0/sim1.schedule[i].g_value*sim1.schedule[i].g_value << " " << temp_ave<< " " << temp_error << std::endl;
+        // }
+
+        // sim1.stringTension("string2go",10);
 
 
         std::cout << '\a';
